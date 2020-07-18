591937f6deb469e066c255e3028e54e73cae4807
nishida <k.nishida@vistage-llc.com>
2020-07-18 21:48:48 +0900

    社員一覧controller.blade

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $user = auth()->user();
+        $this->authorize('viewAny', $user);  // Policy をチェック
+        $users = User::get(); // 社員一覧を取得
+        return view('users.index', compact('users')); // users.index.bldae を呼出し、 usersを渡す
+    }
+}
