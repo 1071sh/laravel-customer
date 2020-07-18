417c7887b776c2a7c7dfe4a4bf12eac0c87a9738
nishida <k.nishida@vistage-llc.com>
2020-07-18 22:32:19 +0900

    クレーマーが登録されたらスーパーバイザーにメールを送る

---
+++ app/Listeners/SendKramerInCommingNotification.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Listeners;
+
+use App\User;
+use Illuminate\Support\Facades\Mail;
+use App\Events\KramerInComming;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendKramerInCommingNotification
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  KramerInComming  $event
+     * @return void
+     */
+    public function handle(KramerInComming $event)
+    {
+        // クレーマーなら通知を発送 。クレーマー確定前に クレーマー変数にカスタマーを入れているのはなんか変だなぁ。
+        if ($event->kramer->isKramer()) {
+            foreach (User::enumSupserVisor() as $superVisor) {
+                Mail::to($superVisor->email)->send(new \App\Mail\KramerInComming($event->kramer));
+                //Mail::to($superVisor->email)->send(\App\Mail\KramerInComming($event->kramer));
+            }
+        }
+    }
+}
